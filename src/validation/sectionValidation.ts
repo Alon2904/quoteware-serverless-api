8b4b7cd3b01f0a2f4fa7cda32a4daadd7035f095
alon2904 <21098015+Alon2904@users.noreply.github.com>
2024-07-26 12:09:47 +0100

    feat: Update sectionValidation to use camelCase naming
    The purpose of this commit is to update the `sectionValidation` module to use camelCase naming for the `createdAt`, `editedAt`, and `quoteId` attributes. This change ensures consistency in attribute naming throughout the codebase.

--- src/validation/sectionValidation.ts
+++ src/validation/sectionValidation.ts
@@ -3,14 +3,15 @@ import Joi from "joi";
 // src/validation/sectionValidation.ts
 
 export const sectionSchema = Joi.object({
-  id: Joi.string().optional(), // This will be generated server-side
-  author: Joi.string().required(),
-  type: Joi.string().valid('project', 'template').required(),
-  name: Joi.string().required(),
-  title: Joi.string().required(),
-  content: Joi.string().required(),
-  index: Joi.number().integer().required(),
-  created_at: Joi.string().optional(), // This will be generated server-side
-  edited_at: Joi.string().optional(), // This will be generated server-side
-  quote_id: Joi.string().optional(), // Optional field
-});
+    id: Joi.string().optional(), // This will be generated server-side if not provided
+    author: Joi.string().required(),
+    type: Joi.string().valid('project', 'template').required(),
+    name: Joi.string().required(),
+    title: Joi.string().required(),
+    content: Joi.string().required(),
+    index: Joi.number().integer().required(),
+    createdAt: Joi.string().optional(), // This will be generated server-side
+    editedAt: Joi.string().optional(), // This will be generated server-side
+    quoteId: Joi.string().optional(), // Optional field
+  });
+
